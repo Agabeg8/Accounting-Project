3e5de476317fc0c25c9eb9a093fc0106d6695e59
Agabeg8 <agabegh@gmail.com>
2023-04-18 14:27:01 +0200

    enums,Role and Address class created

--- src/main/java/com/cydeo/entity/Address.java
+++ src/main/java/com/cydeo/entity/Address.java
@@ -1,4 +1,20 @@
 package com.cydeo.entity;
 
+import jakarta.persistence.Table;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Getter
+@Setter
+@Table(name = "addresses")
 public class Address {
+
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String country;
+    private String zipCode;
 }
